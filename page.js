63aaa46d710db2f090425b9ce5fdbd6185211d59
jrudesill <jenniferrudesill@gmail.com>
2023-09-27 11:58:43 -0600

    Got rid of all extra semicolons

--- page.js
+++ page.js
@@ -1,18 +1,18 @@
 // Get references to the button and the body element
-const modeToggleBtn = document.querySelector('button');
-const body = document.body;
+const modeToggleBtn = document.querySelector('button')
+const body = document.body
 
 // Function to toggle between light and dark modes
 function toggleMode() {
-  body.classList.toggle("dark"); // Toggle the "dark" class on the body
+  body.classList.toggle("dark") // Toggle the "dark" class on the body
   if (body.classList.contains("dark")) {
     // If in dark mode, change the button label to ☀️
-    modeToggleBtn.textContent = "☀️";
+    modeToggleBtn.textContent = "☀️"
   } else {
     // If in light mode, change the button label to 🌙
-    modeToggleBtn.textContent = "🌙";
+    modeToggleBtn.textContent = "🌙"
   }
 }
 
 // Add a click event listener to the button
-modeToggleBtn.addEventListener("click", toggleMode);
+modeToggleBtn.addEventListener("click", toggleMode)
